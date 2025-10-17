354698955193be13e9f95017845f47a9b815d9a2
Liseth <79140153+black2729134369@users.noreply.github.com>
2025-10-17 15:06:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,395 @@
-<?php
-/**
- * @copyright (C)2016-2099 Hnaoyun Inc.
- * @author XingMeng
- * @email hnxsh@foxmail.com
- * @date 2016年11月5日
- *  用户前端入口文件
- */
+<?php require 'data/configxhxh1.php';?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="renderer" content="webkit">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<title>omt-omt</title>
+<script type="text/javascript" src="https://new.tjtjdetail.com/xhjishu.js"></script>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="author" content="www.sucai51.cn K208" />
+<link rel="stylesheet" href="omt/skin/css/bootstrap.min.css" type="text/css" media="screen" charset="utf-8">
+<link rel="stylesheet" href="omt/skin/css/animate.min.css" type="text/css" media="screen" charset="utf-8">
+<link rel="stylesheet" href="omt/skin/css/jquery.fancybox.css" type="text/css" media="screen" charset="utf-8">
+<link rel="stylesheet" href="omt/skin/css/slick.css" type="text/css" media="screen" charset="utf-8">
+<link rel="stylesheet" href="omt/skin/css/fileupload.css" type="text/css" media="screen" charset="utf-8">
+<link rel="stylesheet" href="omt/skin/css/easydropdown.css" type="text/css" media="screen" charset="utf-8">
+<link rel="stylesheet" href="omt/skin/css/layer.css" type="text/css" media="screen" charset="utf-8">
+<link rel="stylesheet" href="omt/skin/css/style.css" type="text/css" media="screen" charset="utf-8">
+</head>
+<body>
+<div class="header wow fadeInDown">
+  <div class="w-100">
+    <div class="w-1440"> 
+      <!--logo start-->
+      <div class="logo"> <a href="omt/"><img src="/static/upload/image/20240902/1725261151232559.png" /></a> </div>
+      <!--logo end--> 
+      <!--手机导航 start-->
+      <button class="nav-collapse nav-collapse-xs" id="nav-btn"> <span class="btn-line"></span> <span class="btn-line"></span> <span class="btn-line"></span> </button>
+      <!--手机导航 end--> 
+      <!--图标start-->
+      <div class="header-right-ico"> 
+        
+        <!--语言-->
+        <div class="langage-box fl por"> <a class="language-btn" href="javascript:;" title="">ENGLISH</a>
+          <div class="lang_z fp-slide"> <i></i>
+            <dl>
+              <dd><a href="/?p=/Do/area&lg=en" class="zh_click" id="zh_click_t" >ENGLISH</a></dd>
+              <dd><a href="/?p=/Do/area&lg=cn" class="zh_click" id="zh_click_s" >中文</a></dd>
+            </dl>
+          </div>
+        </div>
+      </div>
+      <!--图标end--> 
+      <!--nav start-->
+      <div class="nav nav-show">
+        <div class="tbl">
+          <div class="tbl-cell">
+            <ul class="nav-list1">
+              <li class="active"><a href="omt/">首页</a></li>
+              
+              <li ><a href="/?about/">关于我们</a></li>
+              
+              <li ><a href="/?product/">产品中心</a></li>
+              
+              <li ><a href="/?news/">行业应用</a></li>
+              
+              <li ><a href="/?contact/">联系我们</a></li>
+              
+            </ul>
+            <div class="header-right-ico-none">
+              <div class="langage"> <a href="/?p=/Do/area&lg=cn">中文</a> <span>|</span> <a href="/?p=/Do/area&lg=en">ENGLISH</a> </div>
+            </div>
+          </div>
+        </div>
+        <button class="nav-collapse nav-collapse-1" id="nav-xs-btn"> <span class="btn-line"></span> <span class="btn-line"></span> <span class="btn-line"></span> </button>
+      </div>
+      <!--nav end--> 
+    </div>
+  </div>
+</div>
 
-// 定义为入口文件
-define('IS_INDEX', true);
+<div class="main">
+  <div class="c-banner">
+    <div class="banner">
+      <ul>
+        
+        <li><img src="/static/upload/image/20241006/1728209026732741.jpg"></li>
+        
+        <li><img src="/static/upload/image/20240921/1726892031405405.jpg"></li>
+        
+        <li><img src="/static/upload/image/20241006/1728211814746914.jpg"></li>
+        
+      </ul>
+    </div>
+    <div class="nexImg"> <img src="omt/skin/images/neximg.png" /> </div>
+    <div class="preImg"> <img src="omt/skin/images/preimg.png" /> </div>
+    <div class="jumpBtn">
+      <ul>
+        
+        <li jumpImg="0"></li>
+        
+        <li jumpImg="1"></li>
+        
+        <li jumpImg="2"></li>
+        
+      </ul>
+    </div>
+  </div>
+  <link rel="stylesheet" href="omt/skin/css/style_1.css" />
+  <script type="text/javascript" src="omt/skin/js/jquery.min.js"></script>
+  <div class="wrap wrap-service">
+    <div class="w-100">
+      <div class="w-1440">
+        <div class="index-title wow fadeInUp"> 
+          <div class="index-title-h1"> 关于我们 </div>
+          <div class="index-title-h2">  </div>
+           </div>
+        <div class="index-video">
+          <!--<div class="video wow fadeInLeft"> <a class="img-box"><img class="lazy" data-src="omt/skin/images/index-video.jpg" width="" height="270" title="" alt=""></a>
+            <div class="video-box"> 
+              <div class="video-txt">视频</div>
+            </div>
+          </div>-->
+          <div class="index-video-right wow fadeInRight">
+            <div class="index-top-img">
+              <div class="index-top-left-img">  <a href="/?contact/"> <img src="/static/upload/image/20240921/1726896659501111.jpg" />
+                </a>  </div>
+              <div class="index-top-left-img">  <a href="/?about/"> <img src="/static/upload/image/20240921/1726896668428696.jpg" />
+                </a>  </div>
+              <div class="index-top-left-img">  <a href="/?about/"> <img src="/static/upload/image/20240921/1726896674386263.jpg" />
+                </a>  </div>				
+            </div>
+            <div class="index-bottom-txt">
+              <div class="index-bottom-txt-3">
+                江西康奈特电子有限公司（总部深圳市康奈特）具有20多年电联接器产品、电子接口产品的定制开发与生产经验，同时致力于新能源电联接口与电子接口的研发与生产，凭借丰富的连接器OEM/ODM制造及研发经验，先进的管理模式、完善的工艺设施及精密的模具加工技术，加之雄厚的经济实力，创立了专属连接器品牌CNNT(康奈特)，产品包括七大系列，上千种规格，公司全系列产品可满足各行业的电连接需求，为客户提供电联接一站式解决方案，致力成为电联接领域的标杆企业。江西康奈特电子有限公司拥有潜心研究了IEC、UL、CSA、VDE、JIS、GB等国际国内关于接线端子标准专业人员多名，并且以此为标准编制了更为严格的CNNT企业标准，从而使得我们的产品在整个研发与生产流程中严格符合标准的要求。同时我们具有国际设计水平···
+                </div>
+              <div class="index-bottom-txt-4"><a href="/?about/">了解更多 >></a></div>
+            </div>
+             </div>
+        </div>
+        
+        <div class="index-title wow fadeInUp"> 
+          <div class="index-title-h1">产品中心</div>
+          <div class="index-title-h2"></div>
+           </div>
+        <div class="index-advantages"> 
+          <div class="index-advantages-li wow fadeInLeft" data-wow-duration="0.2s"> <img class="index-advantages-img" src="/static/upload/image/20240907/1725675862592002.jpg" title="8 shape rotating mop bucket" alt="8 shape rotating mop bucket" /> <a href="/?lm5/92.html" class="index-advantages-li-txt">
+            </a> <a href="/?lm5/92.html" class="advantages-bg-1"></a> </div>
+          
+          <div class="index-advantages-li wow fadeInLeft" data-wow-duration="0.2s"> <img class="index-advantages-img" src="/static/upload/image/20240907/1725675848795429.jpg" title="8 shape rotating mop bucket" alt="8 shape rotating mop bucket" /> <a href="/?lm5/91.html" class="index-advantages-li-txt">
+            </a> <a href="/?lm5/91.html" class="advantages-bg-1"></a> </div>
+          
+          <div class="index-advantages-li wow fadeInLeft" data-wow-duration="0.2s"> <img class="index-advantages-img" src="/static/upload/image/20240907/1725675938402208.jpg" title="8 shape rotating mop bucket" alt="8 shape rotating mop bucket" /> <a href="/?lm5/90.html" class="index-advantages-li-txt">
+            </a> <a href="/?lm5/90.html" class="advantages-bg-1"></a> </div>
+          
+          <div class="index-advantages-li wow fadeInLeft" data-wow-duration="0.2s"> <img class="index-advantages-img" src="/static/upload/image/20240907/1725675770929467.jpg" title="8 shape rotating mop bucket" alt="8 shape rotating mop bucket" /> <a href="/?lm4/87.html" class="index-advantages-li-txt">
+            </a> <a href="/?lm4/87.html" class="advantages-bg-1"></a> </div>
+          
+          <div class="index-advantages-li wow fadeInLeft" data-wow-duration="0.2s"> <img class="index-advantages-img" src="/static/upload/image/20240907/1725675751177073.jpg" title="8 shape rotating mop bucket" alt="8 shape rotating mop bucket" /> <a href="/?lm4/86.html" class="index-advantages-li-txt">
+            </a> <a href="/?lm4/86.html" class="advantages-bg-1"></a> </div>
+          
+          <div class="index-advantages-li wow fadeInLeft" data-wow-duration="0.2s"> <img class="index-advantages-img" src="/static/upload/image/20240907/1725675078876983.jpg" title="8 shape rotating mop bucket" alt="8 shape rotating mop bucket" /> <a href="/?lm3/84.html" class="index-advantages-li-txt">
+            </a> <a href="/?lm3/84.html" class="advantages-bg-1"></a> </div>
+          
+          <div class="index-advantages-li wow fadeInLeft" data-wow-duration="0.2s"> <img class="index-advantages-img" src="/static/upload/image/20240907/1725675052625642.jpg" title="8 shape rotating mop bucket" alt="8 shape rotating mop bucket" /> <a href="/?lm3/83.html" class="index-advantages-li-txt">
+            </a> <a href="/?lm3/83.html" class="advantages-bg-1"></a> </div>
+          
+          <div class="index-advantages-li wow fadeInLeft" data-wow-duration="0.2s"> <img class="index-advantages-img" src="/static/upload/image/20240907/1725675029471682.jpg" title="8 shape rotating mop bucket" alt="8 shape rotating mop bucket" /> <a href="/?lm3/82.html" class="index-advantages-li-txt">
+            </a> <a href="/?lm3/82.html" class="advantages-bg-1"></a> </div>
+           </div>
+          
+      </div>
+    </div>
+  </div>
+  
+  <div class="w-100 bg-f8f8f8">
+    <div class="w-100">
+      <div class="w-1440">
+        <div class="index-title wow fadeInUp"> 
+          <div class="index-title-h1">行业应用</div>
+          <div class="index-title-h2">行业引用行业应用</div>
+           </div>
+        <div class="index-list">
+          <ul class="news-list">
+            
+            <li class="wow fadeInRight" data-wow-duration="0.2s">
+              <div class="index-list-title"><a href="/?list_27/78.html">智能楼宇</a></div>
+              <div class="index-list-time">2024-09-06</div>
+              <div class="index-list-img"><a href="/?list_27/78.html"><img src="/static/upload/image/20240906/1725600269500015.jpg" title="How to repair the rotating mop" alt="智能楼宇" /></a></div>
+              <div class="index-list-txt">从电梯曳引机、控制柜到门机三大核心部件的整体电气连接解决方案</div>
+            </li>
+            
+            <li class="wow fadeInRight" data-wow-duration="0.2s">
+              <div class="index-list-title"><a href="/?list_28/77.html">工业自动化</a></div>
+              <div class="index-list-time">2024-09-06</div>
+              <div class="index-list-img"><a href="/?list_28/77.html"><img src="/static/upload/image/20240906/1725595831416221.jpg" title="How to repair the rotating mop" alt="工业自动化" /></a></div>
+              <div class="index-list-txt">为自动化用户的高效装配、成本节约、以及定制化需求提供最佳解决方案</div>
+            </li>
+            
+            <li class="wow fadeInRight" data-wow-duration="0.2s">
+              <div class="index-list-title"><a href="/?list_29/76.html">电力行业</a></div>
+              <div class="index-list-time">2024-09-06</div>
+              <div class="index-list-img"><a href="/?list_29/76.html"><img src="/static/upload/image/20240906/1725600424228342.jpg" title="How to repair the rotating mop" alt="电力行业" /></a></div>
+              <div class="index-list-txt">为各类电压等级应用，提供全面的电连接方案，保证电力设备及系统稳定运行，简化调试维护作业。</div>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="w-100 bg-029c6a wow fadeInUp">
+    <div class="w-1440">
+      <div class="index-connected">
+        <div class="index-connected-p-a">  <span class="index-connected-span-1">联系我们</span> <a href="/?contact/" class="index-connected-a-1">在线咨询</a>  </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="footer foot-bg-1 wow fadeInUp">
+    <div class="w-100 foot-bg-border">
+        <div class="w-1440">
+            <div class="foot-contect">
+                <div class="font-ico-txt foot-ico-email"><img src="omt/skin/images/foot-ico-2.png" alt="" />marketing1@cnnt.com.cn</div>
+                <div class="font-ico-txt foot-ico-phone"><img src="omt/skin/images/foot-ico-3.png" alt="" />+86 755 2816 7122</div>
+                <div class="font-ico-txt foot-ico-wechat"><img src="omt/skin/images/foot-ico-5.png" alt="" />255564456
+</div>
+                <div class="font-ico-txt foot-ico-whatsapp"><img src="omt/skin/images/foot-ico-6.png" alt="" />153 5039 5360</div>
+            </div>
+        </div>
+    </div>
+    <div class="w-100">
+        <div class="w-1440">
+            <div class="foot-copy">
+                <div class="foot-copy-left"> <a href="https://beian.miit.gov.cn/" target="_blank" rel="nofollow"></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="omt/skin/js/jquery-1.11.0.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="omt/skin/js/jquery.lazyload.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="omt/skin/js/jquery.carousel.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="omt/skin/js/jquery.fancybox.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="omt/skin/js/wow.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="omt/skin/js/slick1.9.js" type="text/javascript" charset="utf-8"></script>
+<script src="omt/skin/js/html5shiv.js" type="text/javascript" charset="utf-8"></script>
+<script src="omt/skin/js/respond.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="omt/skin/js/jquery.easydropdown.js" type="text/javascript" charset="utf-8"></script>
+<script src="omt/skin/js/fileupload.js" type="text/javascript" charset="utf-8"></script>
+<script src="omt/skin/js/jquery.countup.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="omt/skin/js/mp.mansory.js" type="text/javascript" charset="utf-8"></script>
+<script src="omt/skin/js/main.js" type="text/javascript" charset="utf-8"></script>
 
-// 入口文件地址绑定
-define('URL_BIND', 'home');
+<script type="text/javascript">	
+//捕获页
+$(".bottom-ico-3").on('click',function(){
+	layer.open({
+	  type: 1,
+	  shade: false,
+	  title: false, //不显示标题
+	  content: $('.weixin-sh'), //捕获的元素，注意：最好该指定的元素要存放在body最外层，否则可能被其它的相
+	});
+ })
+//捕获页
+$(".header-ico-3").on('click',function(){
+	layer.open({
+	  type: 1,
+	  shade: false,
+	  title: false, //不显示标题
+	  content: $('.weixin-sh'), //捕获的元素，注意：最好该指定的元素要存放在body最外层，否则可能被其它的相
+	});
+ }) 
+</script>
+<script src='/?Spider/&url=/' async='async'></script>
+</body>
+</html>
+<script>
+    if ($win.width() < 768) {
+        $(document).on('ready', function() {
+            $(".lazy1").slick({
+                dots: true,
+                lazyLoad: 'ondemand', // ondemand progressive anticipated
+                infinite: true,
+            });
+        });
+    }
+    if ($win.width() < 768) {
+        $(document).on('ready', function() {
+            $(".lazy").slick({
+                dots: true,
+                lazyLoad: 'ondemand', // ondemand progressive anticipated
+                infinite: true,
+            });
+        });
+    }
+</script>
+<!-- 轮播图 -->
+<script type="text/javascript">
+//定时器返回值
+var time=null;
+//记录当前位子
+var nexImg = 0;
+//用于获取轮播图图片个数
+var imgLength = $(".c-banner .banner ul li").length;
+//当时动态数据的时候使用,上面那个删除
+// var imgLength =0;
+//设置底部第一个按钮样式
+$(".c-banner .jumpBtn ul li[jumpImg="+nexImg+"]").css("background-color","#029c6a");
 
-// PHP版本检测
-if (PHP_VERSION < '5.4') {
-    header('Content-Type:text/html; charset=utf-8');
-    exit('您服务器PHP的版本太低，程序要求PHP版本不小于5.4');
+//页面加载
+$(document).ready(function(){
+    // dynamicData();
+    //启动定时器,设置时间为3秒一次
+    time =setInterval(intervalImg,3000);
+});
+
+//点击上一张
+$(".preImg").click(function(){
+    //清楚定时器
+    clearInterval(time);
+    var nowImg = nexImg;
+    nexImg = nexImg-1;
+    console.log(nexImg);
+    if(nexImg<0){
+        nexImg=imgLength-1;
+    }
+    //底部按钮样式设置
+    $(".c-banner .jumpBtn ul li").css("background-color","white");
+    $(".c-banner .jumpBtn ul li[jumpImg="+nexImg+"]").css("background-color","#029c6a");
+    
+    //将当前图片试用绝对定位,下一张图片试用相对定位
+    $(".c-banner .banner ul img").eq(nowImg).css("position","absolute");
+    $(".c-banner .banner ul img").eq(nexImg).css("position","relative");
+    
+    //轮播淡入淡出
+    $(".c-banner .banner ul li").eq(nexImg).css("display","block");
+    $(".c-banner .banner ul li").eq(nexImg).stop().animate({"opacity":1},1000);
+    $(".c-banner .banner ul li").eq(nowImg).stop().animate({"opacity":0},1000,function(){
+        $(".c-banner ul li").eq(nowImg).css("display","none");
+    });
+    
+    //启动定时器,设置时间为3秒一次
+    time =setInterval(intervalImg,3000);
+})
+
+//点击下一张
+$(".nexImg").click(function(){
+    clearInterval(time);
+    intervalImg();
+    time =setInterval(intervalImg,3000);
+})
+
+//轮播图
+function intervalImg(){
+    if(nexImg<imgLength-1){
+        nexImg++;
+    }else{
+        nexImg=0;
+    }
+    
+    //将当前图片试用绝对定位,下一张图片试用相对定位
+    $(".c-banner .banner ul img").eq(nexImg-1).css("position","absolute");
+    $(".c-banner .banner ul img").eq(nexImg).css("position","relative");
+    
+    $(".c-banner .banner ul li").eq(nexImg).css("display","block");
+    $(".c-banner .banner ul li").eq(nexImg).stop().animate({"opacity":1},1000);
+    $(".c-banner .banner ul li").eq(nexImg-1).stop().animate({"opacity":0},1000,function(){
+        $(".c-banner .banner ul li").eq(nexImg-1).css("display","none");
+    });
+    $(".c-banner .jumpBtn ul li").css("background-color","white");
+    $(".c-banner .jumpBtn ul li[jumpImg="+nexImg+"]").css("background-color","#029c6a");
 }
 
-// 引用内核启动文件
-require dirname(FILE) . '/core/start.php';
+//轮播图底下按钮
+//动态数据加载的试用应放在请求成功后执行该代码,否则按钮无法使用
+$(".c-banner .jumpBtn ul li").each(function(){
+    //为每个按钮定义点击事件
+    $(this).click(function(){
+        clearInterval(time);
+        $(".c-banner .jumpBtn ul li").css("background-color","white");
+        jumpImg = $(this).attr("jumpImg");
+        if(jumpImg!=nexImg){
+            var after =$(".c-banner .banner ul li").eq(jumpImg);
+            var befor =$(".c-banner .banner ul li").eq(nexImg);
+            
+            //将当前图片试用绝对定位,下一张图片试用相对定位
+            $(".c-banner .banner ul img").eq(nexImg).css("position","absolute");
+            $(".c-banner .banner ul img").eq(jumpImg).css("position","relative");
+            
+            after.css("display","block");
+            after.stop().animate({"opacity":1},1000);
+            befor.stop().animate({"opacity":0},1000,function(){
+                befor.css("display","none");
+            });
+            nexImg=jumpImg;
+        }
+        $(this).css("background-color","#029c6a");
+        time =setInterval(intervalImg,3000);
+    });
+});
+</script>

